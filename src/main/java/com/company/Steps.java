0e0c63175fc13f158b3942f6b0b5206dbf284c40
Voman68 <nvoma@hotmail.co.uk>
2021-06-22 21:13:19 +0100

    Second Commit

---
+++ src/main/java/com/company/Steps.java
@@ -0,0 +1,32 @@
+package com.company;
+import cucumber.api.java.en.*;
+
+public class Steps {
+    @Given("that a user can verify that the city of Frankfurt is in Germany")
+    public void thatAUserCanVerifyThatTheCityOfFrankfurtIsInGermany() {
+    }
+
+    @And("that we can return the city’s corresponding longitude and latitude")
+    public void thatWeCanReturnTheCitySCorrespondingLongitudeAndLatitude() {
+    }
+
+    @When("a user enters the city Frankfurt after network on the API link and clicks to search")
+    public void aUserEntersTheCityFrankfurtAfterNetworkOnTheAPILinkAndClicksToSearch() {
+    }
+
+    @Then("the location of the city Frankfurt appears, with Germany as the corresponding country")
+    public void theLocationOfTheCityFrankfurtAppearsWithGermanyAsTheCorrespondingCountry() {
+    }
+
+    @And("the city’s corresponding latitude and longitude appears")
+    public void theCitySCorrespondingLatitudeAndLongitudeAppears() {
+    }
+
+    @And("the company name with the file reference and its’ ID appears")
+    public void theCompanyNameWithTheFileReferenceAndItsIDAppears() {
+    }
+
+    @And("the name of Station displayed")
+    public void theNameOfStationDisplayed() {
+    }
+}
